71e931d61374c9f097036eb5c83df0180c88f5d0
Rana Awab <mawabjaved@gmail.com>
2026-07-23 13:42:33 +0500

    Update frontend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://postgres-crud-tdpg.onrender.com",
+  baseURL: "https://postgres-crud-backend.onrender.com",
 });
 
 export default api;
