e00954c15a4e8ccda067f4b43c40f3b7c298f73b
Rana Awab <mawabjaved@gmail.com>
2026-07-23 13:18:18 +0500

    Add home route for backend

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Home Route
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "PostgreSQL CRUD API is running successfully."
+    });
+});
+
+// Student Routes
 app.use("/students", studentRoutes);
 
 const PORT = process.env.PORT || 5000;
